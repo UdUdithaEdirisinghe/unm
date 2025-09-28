5da1caf97c6a79c1459f9c73dbd75949feefa9b7
UdUdithaEdirisinghe <udithaedirisinghe69@gmail.com>
2025-09-28 13:32:34 +0530

    instaLink

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -76,7 +76,7 @@ export default function Footer() {
             <div className="mt-3 flex items-center justify-center gap-4 md:justify-start">
               {/* Instagram */}
               <a
-                href="https://instagram.com/@manny.lk.store"
+                href="https://www.instagram.com/manny.lk.store?igsh=MTR5cGpwcDUzcTA5Mw%3D%3D&utm_source=qr"
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="Instagram"
